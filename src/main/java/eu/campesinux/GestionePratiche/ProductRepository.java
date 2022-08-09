4b35d74d8b09ef59496607e3d5bf345a34946d81
campesinux <corrado.campisano@gmail.com>
2022-08-09 15:34:35 +0200

    CRUD replicato su gestione pratiche, eliminati riferimenti a products

--- src/main/java/eu/campesinux/GestionePratiche/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package eu.campesinux.GestionePratiche;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
