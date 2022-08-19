99bfacb107d4001e863de0cc279e3ff81bcb5552
campesinux <corrado.campisano@gmail.com>
2022-08-19 17:23:23 +0200

    prima implementazione effettiva della gestione, anche automatica, delle scadenze

--- src/main/java/eu/campesinux/GestionePratiche/quartzJobs/PraticheScadenzeJob.java
+++ src/main/java/eu/campesinux/GestionePratiche/quartzJobs/PraticheScadenzeJob.java
@@ -14,6 +14,8 @@ import eu.campesinux.GestionePratiche.avanzamenti.Avanzamento;
 import eu.campesinux.GestionePratiche.avanzamenti.AvanzamentoService;
 import eu.campesinux.GestionePratiche.pratiche.Pratica;
 import eu.campesinux.GestionePratiche.pratiche.PraticaService;
+import eu.campesinux.GestionePratiche.statoPratica.StatoPratica;
+import eu.campesinux.GestionePratiche.statoPratica.StatoPraticaService;
 
 public class PraticheScadenzeJob extends QuartzJobBean {
 
@@ -25,33 +27,82 @@ public class PraticheScadenzeJob extends QuartzJobBean {
 	private PraticaService pService;
 	@Autowired
 	private AvanzamentoService aService;
+	@Autowired
+	private StatoPraticaService spService;
 	
 	@Override
 	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
 
 		JobDataMap jobDataMap = context.getMergedJobDataMap();
 		Long giorniAnticipo = jobDataMap.getLong("giorniAnticipo");
-		System.out.println("Esecuzione del job con parametro giorniAnticipo = " + giorniAnticipo);
+		System.out.println("Esecuzione del job con parametro 'giorniAnticipo' = " + giorniAnticipo);
 		
 		List<Pratica> praticheAperte = pService.listAperte();
 		for (Pratica pratica : praticheAperte) {
-			System.out.println("Processamento della pratica" + pratica.getIdentificativo());
+			System.out.println("Processamento della pratica: " + pratica.getIdentificativo());
 			List<Avanzamento> avanzamenti = aService.listByPratica(pratica);
 			LocalDateTime dataAvanzamentoMax=null;
 			LocalDateTime dataScadenzaMax=null;
+			StatoPratica statoPrecedente=null;
 			for (Avanzamento avanzamento : avanzamenti) {
 				if (dataAvanzamentoMax==null) {
 					dataAvanzamentoMax = avanzamento.getData(); 
 					dataScadenzaMax = avanzamento.getScadenza();
+					statoPrecedente = avanzamento.getStatoAttuale();
 				} else {
 					if (avanzamento.getData().isAfter(dataAvanzamentoMax)) {
 						dataAvanzamentoMax = avanzamento.getData(); 
 						dataScadenzaMax = avanzamento.getScadenza();
+						statoPrecedente = avanzamento.getStatoAttuale();
 					}					
 				}
 			}
-			System.out.println("L'ultimo avanzamento e' in data" + dataAvanzamentoMax);
-			System.out.println("La relativa data di scadenza e'" + dataScadenzaMax);
+			System.out.println("L'ultimo avanzamento e' in data: " + dataAvanzamentoMax);
+			System.out.println("La relativa data di scadenza e': " + dataScadenzaMax);
+			
+			// pratiche da mettere in scadenza
+			// se l'ultima data di scadenza e' prima di oggi + giorniAnticipo
+			LocalDateTime oggi = LocalDateTime.now();
+			oggi.plusDays(giorniAnticipo);
+			if(dataScadenzaMax.isBefore(oggi.plusDays(giorniAnticipo))) {
+				
+				System.out.println("la pratica verra' messa in scadenza");
+				
+				// crea un nuovo avanzamento 
+				Avanzamento inScadenza = new Avanzamento();
+				inScadenza.setPratica(pratica);
+				inScadenza.setDescrizione("Pratica messa 'in scadenza' in automatico");
+				inScadenza.setData(oggi);
+				inScadenza.setScadenza(dataScadenzaMax);
+				StatoPratica statoAttuale = spService.findByStato(StatoPratica.STATO_IN_SCADENZA);
+				inScadenza.setStatoAttuale(statoAttuale);
+				inScadenza.setStatoPrecedente(statoPrecedente);
+				aService.save(inScadenza);
+				// aggiorna lo stato della pratica
+				pratica.setStato(statoAttuale);
+				pService.save(pratica);
+				
+			}
+			// pratiche da mettere in scadute
+			// se l'ultima data di scadenza e' prima di oggi
+			if(dataScadenzaMax.isBefore(oggi)) {
+				
+				System.out.println("la pratica verra' considerata scaduta");
+				
+				// crea un nuovo avanzamento 
+				Avanzamento inScadenza = new Avanzamento();
+				inScadenza.setPratica(pratica);
+				inScadenza.setDescrizione("Pratica divenuta 'scaduta' in automatico");
+				inScadenza.setData(oggi);
+				inScadenza.setScadenza(dataScadenzaMax);
+				StatoPratica statoAttuale = spService.findByStato(StatoPratica.STATO_SCADUTA);
+				inScadenza.setStatoAttuale(statoAttuale);
+				inScadenza.setStatoPrecedente(statoPrecedente);
+				aService.save(inScadenza);
+				// aggiorna lo stato della pratica
+				pratica.setStato(statoAttuale);
+				pService.save(pratica);
+			}
 		}
 	}
 
